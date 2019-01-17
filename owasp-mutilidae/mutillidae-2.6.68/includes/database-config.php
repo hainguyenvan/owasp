7e651fba5c7a236fa50aaf217f76a4d5ad3fa610
hainv <hai213k57@gmail.com>
2019-01-17 23:10:16 +0700

    Fix bug error add mutilidea
    Signed-off-by: hainv <hai213k57@gmail.com>

---
+++ owasp-mutilidae/mutillidae-2.6.68/includes/database-config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', getenv('MYSQL_HOST'));
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', getenv('MYSQL_ROOT_PASSWORD'));
+define('DB_NAME', getenv('MYSQL_DATABASE'));
+?>
